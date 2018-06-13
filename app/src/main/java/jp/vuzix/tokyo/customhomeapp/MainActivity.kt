14191bdfc262aa8c79ddbd0746b1769a1d3680f0
Keiji ARIYAMA <keiji_ariyama@c-lis.co.jp>
2018-06-13 11:26:13 +0900

    Add custom launcher function.

--- app/src/main/java/jp/vuzix/tokyo/customhomeapp/MainActivity.kt
+++ app/src/main/java/jp/vuzix/tokyo/customhomeapp/MainActivity.kt
@@ -1,12 +1,28 @@
 package jp.vuzix.tokyo.customhomeapp
 
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val SETTINGS_PACKAGE_NAME = "com.android.settings"
+        val SETTINGS_CLASS_NAME = "com.android.settings.Settings"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_open_setting.setOnClickListener({
+            val intent = Intent().apply {
+                setClassName(SETTINGS_PACKAGE_NAME, SETTINGS_CLASS_NAME)
+            }
+            startActivity(intent)
+        })
+
     }
 }
